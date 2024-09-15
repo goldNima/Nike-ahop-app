d95c341e53568f626b566e9e0e8c64bfb18fb94e
Nima <165915862+goldNima@users.noreply.github.com>
2024-09-15 04:43:29 -0700

    Update dashboard.jsx

--- src/routers/dashboard.jsx
+++ src/routers/dashboard.jsx
@@ -27,12 +27,12 @@ export function Dashboard (){
                 <div className='left-dashboard'>
                     <h1 className='title-dashboard'>Dashboard</h1>
                     <br></br>
-                    <img onClick={Home} className='logo-dashboard-left1' src={home}></img>
+                    <img alt='img' onClick={Home} className='logo-dashboard-left1' src={home}></img>
                     <br></br>
-                    <img onClick={cart} className='logo-dashboard-left2' src={store}></img>
+                    <img alt='img' onClick={cart} className='logo-dashboard-left2' src={store}></img>
                 </div>
                 <div className='right-dashboard'>
-                    <img className='img-right-prof' src={profile}></img>
+                    <img alt='img' className='img-right-prof' src={profile}></img>
                     <h3 className='info-user'>User Name : {name}</h3>
                     <h3 className='info-user'>User lastname : {lastname}</h3>
                     <h3 className='info-user'>USer password : {password}</h3>
